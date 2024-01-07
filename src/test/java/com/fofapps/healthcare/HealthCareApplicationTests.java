588c635e65661b3985bf85b01d31fb6eb8ab2d57
williansql <williansql@outlook.com>
2024-01-07 01:01:42 -0300

    inicio do projeto health care

---
+++ src/test/java/com/fofapps/healthcare/HealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fofapps.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
